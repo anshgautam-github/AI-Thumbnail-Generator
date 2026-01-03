53c1957af2727023ea9604ba019f36547d88dd47
anshgautam-github <anshgautam1011@gmail.com>
2026-01-03 09:45:39 +0530

    updated app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 import LenisScroll from "./components/LenisScroll";
+import Generate from "./pages/Generate";
+import MyGeneration from "./pages/MyGeneration";
+import YtPreview from "./pages/YtPreview";
 
 export default function App() {
     return (
@@ -12,6 +15,10 @@ export default function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/generate" element={<Generate />} />
+                <Route path="/generate/:id" element={<Generate />} />
+                <Route path="/my-generation" element={<MyGeneration />} />
+                <Route path="/preview" element={<YtPreview />} />
             </Routes>
             <Footer />
         </>
