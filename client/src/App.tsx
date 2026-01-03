2b043f196860598cb212bfd0badf89ceee3c9651
anshgautam-github <anshgautam1011@gmail.com>
2026-01-03 09:47:43 +0530

    app routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import LenisScroll from "./components/LenisScroll";
 import Generate from "./pages/Generate";
 import MyGeneration from "./pages/MyGeneration";
 import YtPreview from "./pages/YtPreview";
+import Login from "./components/Login";
 
 export default function App() {
     return (
@@ -19,6 +20,7 @@ export default function App() {
                 <Route path="/generate/:id" element={<Generate />} />
                 <Route path="/my-generation" element={<MyGeneration />} />
                 <Route path="/preview" element={<YtPreview />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
             <Footer />
         </>
